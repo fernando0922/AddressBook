2f794caa921c26f3b67b33a1093c3b6e850fa97e
fernando0922 <fernando.vidal.uw@gmail.com>
2021-12-19 08:48:35 +0530

    404 page done

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import 'antd/dist/antd.css';
+import { Button, Result } from 'antd'
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+
+    let navigate = useNavigate();
+
     return (
-        <div>
-            I AM NOT FOUND
+        <div className="not-found-container">
+        <Result
+        status="404"
+        title="The page you visited does not exist."
+        extra={<Button type="primary" onClick={()=>navigate("/")}>Back To Login</Button>}
+        />
         </div>
     )
 }
