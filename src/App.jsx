b019394a0c519e15b6e25f5dc8204b99d29e1edf
fernando0922 <fernando.vidal.uw@gmail.com>
2021-12-19 07:52:29 +0530

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+import NotFound from "./pages/notFound/NotFound";
+import Detail from "./pages/detail/Detail";
 
 function App() {
   return (
     <div className="App">
-     I AM APP.JS COMPONENT
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="list" element={<Dashboard />} />
+          <Route path="detail" element={<Detail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
