9675b1fdd1c98f3c1adc08c2985d303b421d554b
fernando0922 <fernando.vidal.uw@gmail.com>
2021-12-19 15:23:26 +0530

    Static UI + CSS + RESPONSIVE done
    Next Front ka back aka logic

--- src/pages/dashboard/UserOperation.jsx
+++ src/pages/dashboard/UserOperation.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import "antd/dist/antd.css";
+import { Input } from "antd";
+import AddModal from "./add/AddModal";
+
+
+const { Search } = Input;
 
 const UserOperation = () => {
+  const onSearch = (value) => console.log("value");
+
   return (
     <>
-      <div className="search-box"> SEARCH BOX</div>
-      <div className="add-user"> ADD USER</div>
+      <div className="search-container">
+        <Search
+          placeholder="input search text"
+          onSearch={onSearch}
+          allowClear
+      enterButton="Search"
+        />
+      </div>
+      <AddModal />
     </>
   );
 };
--- src/pages/dashboard/add/AddModal.jsx
+++ src/pages/dashboard/add/AddModal.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Button, Modal, Tooltip } from "antd";
+import { PlusOutlined } from "@ant-design/icons/lib/icons";
 
 const AddModal = () => {
-    return (
-        <div>
-            I AM ADD MODAL
-        </div>
-    )
-}
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-export default AddModal
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button
+        icon={<PlusOutlined />}
+        style={{ backgroundColor: "#0076e3", color:"white" }}
+        onClick={showModal}
+      >
+        ADD CONTACT
+      </Button>
+      <Modal
+        title="EDIR CONTACT INFORMATION"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>YOU SURE ABOUT DELETING??</p>
+      </Modal>
+    </>
+  );
+};
+
+export default AddModal;
